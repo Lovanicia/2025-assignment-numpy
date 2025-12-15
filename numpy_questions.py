da4d61272dea397a747096331edbd90512c1ee21
Lovanicia <lovanicia@gmail.com>
2025-12-15 16:10:06 +0100

    assignment1 finished

--- numpy_questions.py
+++ numpy_questions.py
@@ -39,9 +39,6 @@ def max_index(X):
     """
     i = 0
     j = 0
-
-    # TODO
-    
     if not isinstance(X, np.ndarray):
         raise ValueError("X must be a numpy ndarray")
     if X.ndim != 2:
@@ -58,7 +55,7 @@ def max_index(X):
                 i, j = row, col
 
     return i, j
-    
+
 
 def wallis_product(n_terms):
     """Implement the Wallis product to compute an approximation of pi.
@@ -79,17 +76,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    
-    
     if not isinstance(n_terms, int) or n_terms < 0:
         raise ValueError("n_terms must be positive")
-
     product = 1
     for k in range(1, n_terms + 1):
         num = 4 * k * k
         den = num - 1
         product *= (num / den)
-
     return 2 * product
-
- 
